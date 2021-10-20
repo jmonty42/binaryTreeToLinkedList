5aa9557910968162a57fc7e4ea0c850992f3fa77
J.J. Montgomery <jmonty42.dev@gmail.com>
2021-10-20 10:36:19 -0700

    Added documentation and simplified the tree parsing
    For each method I added documentation on what that method does.
    
    I simplified the tree parsing by getting rid of the startIndex parameter and just dealing with substrings when parsing the right subtree.
    
    TODO: Add a README

--- src/Solution.java
+++ src/Solution.java
@@ -32,10 +32,70 @@ public class Solution {
         public Node right = null;
         public int data;
 
+        /**
+         * Initializes a single node with the given data.
+         *
+         * @param data
+         */
         public Node(int data) {
             this.data = data;
         }
 
+        /**
+         * Create a tree from a String
+         *
+         * String must match the format of the toString() method:
+         * $DATA($LEFT)($RIGHT)
+         *
+         * The string cannot contain whitespace and currently only supports positive integers.
+         *
+         * @param tree
+         */
+        public Node(String tree) {
+            if (tree == null || tree.length() == 0) {
+                throw new RuntimeException("Cannot initialize node from empty or null string.");
+            }
+            Pattern pattern = Pattern.compile("^(\\d+)(.*)$");
+            Matcher matcher = pattern.matcher(tree);
+            if (!matcher.find()) {
+                throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
+            }
+            data = Integer.parseInt(matcher.group(1));
+            String children = matcher.group(2);
+            if (children.length() > 0) {
+                int leftChildEndIndex;
+                try {
+                    leftChildEndIndex = getChildEndIndex(children);
+                    String leftChildString = children.substring(1,leftChildEndIndex);
+                    if (leftChildString.length() > 0) {
+                        left = new Node(leftChildString);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
+                }
+                children = children.substring(leftChildEndIndex+1);
+                try {
+                    // This should always return children.length()-1, but the method also ensures that the substring's
+                    // parentheses are balanced.
+                    int rightChildEndIndex = getChildEndIndex(children);
+                    String rightChildString = children.substring(1, rightChildEndIndex);
+                    if (rightChildString.length() > 0) {
+                        right = new Node(rightChildString);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
+                }
+            }
+        }
+
+        /**
+         * Inserts an integer into the binary search tree whose root is the current node. Duplicates are handled by
+         * inserting them into the left subtree. This would make deleting nodes problematic ... but since that's not
+         * really the point of this project, I'll ignore that. This was implemented purely to enable randomly-generated
+         * trees.
+         *
+         * @param newData
+         */
         public void insert(int newData) {
             if (newData <= data) {
                 if (left == null) {
@@ -60,6 +120,13 @@ public class Solution {
             }
         }
 
+        /**
+         * Returns the maximum depth of the tree rooted at the current node.
+         *
+         * Handles the doubly-linked list case by ignoring each node's left child.
+         *
+         * @return
+         */
         public int maxDepth() {
             int depth = 1;
             //accounts for the case where the tree was converted to a doubly-linked list
@@ -74,8 +141,10 @@ public class Solution {
          *
          * Note: The current Node will not be the head of the resulting linked list unless the left child is null.
          *
-         * Also note: Once this method has been called on a node, calling toString() for that node will result in a
-         * stack overflow.
+         * The current implementation of this is not very efficient because at every node we are retracing each of its
+         * subtrees to either find the head or the tail to attach the current node to the right spot. I think this can
+         * be refactored to return a Node object where by default it will be the new head of the LinkedList, but with a
+         * flag can be the tail.
          */
         public void convertToLinkedList() {
             if (right != null) {
@@ -99,53 +168,45 @@ public class Solution {
         }
 
         /**
-         * Create a tree from a String
-         *
-         * String must match the format of the toString() method:
+         * This method is used when parsing a tree from a string. Each node in the tree is represented by a string
+         * in the format with no whitespace:
          * $DATA($LEFT)($RIGHT)
+         * Where $DATA is a positive integer, $LEFT is a string representation of the left subtree and $RIGHT is a
+         * string representation of the right subtree. Leaf nodes can be depicted with just their $DATA value and no
+         * parentheses for their children.
+         * When the $DATA value is parsed from the string, the remaining string beginning at the next '(' character is
+         * passed to this method first for the left child and then for the right child.
+         * This method will find and return the index of the ')' that represents the string representation of a subtree.
          *
-         * @param tree
+         * Example:
+         * Given a tree represented by the string "3(1()(2))()":
+         *      3
+         *     /
+         *    1
+         *     \
+         *      2
+         * This method will first be called after the root value's data is parsed out. It will be called with the
+         * substring "(1()(2))()" to find the left child's string representation. This method ensures that the first
+         * character of the given string is a left parenthesis '(' and then returns the index of the next balanced right
+         * parenthesis ')', which in this example is index 7.
+         * This indicates that the left child of the root node of this tree is represented by the string inside the
+         * parentheses at index 0 and index 7, or the string "1()(2)".
+         * After the tree parsing method handles the left subtree of the root node, it will then call this method with
+         * the remainder of the original string to find the index bounds of the right subtree of the root node. When
+         * parsing the right child of a given string, the return value should always be the last index of the string.
+         * But this method will also be checking that the string representing a given subtree only contains balanced
+         * parentheses.
+         * In this example, this method will then be called with the string "()" representing the empty right subtree of
+         * the root node. It will return the index 1. When the tree parsing method retrieves the substring between these
+         * parentheses, it will get an empty string and know that this subtree is empty.
+         * @param children
+         * @return
          */
-        public Node(String tree) {
-            if (tree == null || tree.length() == 0) {
-                throw new RuntimeException("Cannot initialize node from empty or null string.");
-            }
-            Pattern pattern = Pattern.compile("^(\\d+)(.*)$");
-            Matcher matcher = pattern.matcher(tree);
-            if (!matcher.find()) {
-                throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
-            }
-            data = Integer.parseInt(matcher.group(1));
-            String children = matcher.group(2);
-            if (children.length() > 0) {
-                int leftChildEndIndex;
-                try {
-                    leftChildEndIndex = getChildEndIndexFromStartIndex(children, 0);
-                    String leftChildString = children.substring(1,leftChildEndIndex);
-                    if (leftChildString.length() > 0) {
-                        left = new Node(leftChildString);
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
-                }
-
-                try {
-                    int rightChildEndIndex = getChildEndIndexFromStartIndex(children, leftChildEndIndex + 1);
-                    String rightChildString = children.substring(leftChildEndIndex + 2, rightChildEndIndex);
-                    if (rightChildString.length() > 0) {
-                        right = new Node(rightChildString);
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException("Cannot initialize node from malformed string: " + tree);
-                }
-            }
-        }
-
-        private static int getChildEndIndexFromStartIndex(String children, int startIndex) {
-            if (children.charAt(startIndex) != '(') {
+        private static int getChildEndIndex(String children) {
+            if (children.charAt(0) != '(') {
                 throw new RuntimeException();
             }
-            int childEndIndex = startIndex+1;
+            int childEndIndex = 1;
             int leftBrackets = 0;
             while (childEndIndex < children.length()) {
                 if (children.charAt(childEndIndex) == ')') {
@@ -165,6 +226,13 @@ public class Solution {
             return childEndIndex;
         }
 
+        /**
+         * Returns a string representing the in-order traversal of the tree rooted at the current node.
+         *
+         * If this is called on a tree that was converted to a doubly-linked list, it will simply ignore each node's
+         * left child, which will still produce an in-order traversal since the doubly-linked list should be in order.
+         * @return
+         */
         public String inOrderString() {
             //accounts for the case where the tree was converted to a doubly-linked list
             String leftString = left == null || left.right == this ? "" : left.inOrderString() + ", ";
@@ -172,13 +240,38 @@ public class Solution {
             return leftString + data + rightString;
         }
 
+        /**
+         * Returns a string representing the doubly-linked list beginning at the current node.
+         *
+         * @return
+         */
         public String linkedListString() {
+            if (right != null && right.left != this) {
+                throw new RuntimeException("linkedListString should not be called on a tree that has not been converted to a doubly-linked list.");
+            }
             String children = right == null ? "" : " <==> " + right.linkedListString();
             return data + children;
         }
 
+        /**
+         * Returns the string representation of the tree rooted at the current node in the format:
+         *
+         * $DATA($LEFT)($RIGHT)
+         *
+         * If the left and right subtrees are both null, it will just return the $DATA element.
+         *
+         * This string can be given to the constructor to initialize an identical tree.
+         *
+         * If this is called on a tree that was converted to a doubly-linked list, it will ignore each node's left
+         * child to avoid a stack overflow.
+         *
+         * @return
+         */
         public String toString() {
-            if (left == null && right == null) {
+            if (right == null && (left == null || left.right == this)) {
+                // The last element in a doubly-linked list with more than one element will not have a null left child.
+                // This ensures that the last element in that case is not represented with two empty parentheses like:
+                // $DATA()()
                 return String.valueOf(data);
             }
             String leftString;
